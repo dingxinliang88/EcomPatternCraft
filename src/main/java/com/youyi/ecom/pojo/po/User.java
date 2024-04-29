6f9bc57160e4342186c2d0a586498e1e84b70916
youyi <youyichannel@163.com>
2024-04-29 19:14:11 +0800

    build(init): add base features

---
+++ src/main/java/com/youyi/ecom/pojo/po/User.java
@@ -0,0 +1,32 @@
+package com.youyi.ecom.pojo.po;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String pwd;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, name = "create_time")
+    private LocalDateTime createTime;
+}
